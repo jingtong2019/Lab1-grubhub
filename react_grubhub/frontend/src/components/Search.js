bb6e97dd80046d09a5abb3292fa0a0c0a13e9fba
jingtong2019 <jing.tong@sjsu.edu>
2019-11-08 19:24:30 -0800

    add pagination

--- react_grubhub/frontend/src/components/Search.js
+++ react_grubhub/frontend/src/components/Search.js
@@ -3,6 +3,8 @@ import {Redirect} from 'react-router';
 import axios from 'axios';
 import './Search.css';
 import {Link} from 'react-router-dom';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 
 class Search extends Component {
@@ -10,7 +12,9 @@ class Search extends Component {
         super(props);
         this.state = {
             to_search: localStorage.getItem("to_search"),
-            cuisine_type: "All"
+            cuisine_type: "All",
+            size: 5,
+            which_page: 1
         };
     }
 
@@ -54,6 +58,76 @@ class Search extends Component {
             select.push(<option value={this.state.cuisine_list[i]}>{this.state.cuisine_list[i]}</option>);
         }
         return select;
+    }
+
+    choosePage(i) {
+        this.setState({which_page: i});
+    }
+
+    helper(pages, info) {
+        let result = [];
+        for (let i = 1; i <= pages; i++) {
+            result.push(
+                <button onClick={() => this.choosePage(i)}>{i}</button>
+            );
+        }
+        result.push(
+            <div>
+                <table class="table">
+                    {info[this.state.which_page]}
+                </table>
+            </div>
+        );
+        return result;
+    }
+
+    createPages = () => {
+        let page_info = {};
+        let cur = 1;
+        let table = [];
+        // table.push(
+        //     <tr>
+        //         <td>Item</td>
+        //         <td>Restaurant</td>
+        //         <td>Cuisine</td>
+        //     </tr>
+        // );
+        let total = 0;
+        for (let i=0; i< this.state.result_number; i++) {
+            if (this.state.cuisine_type === "All" || this.state.result_list[i].cuisine === this.state.cuisine_type) {
+                let children = [];
+                total += 1;
+                children.push(
+                    <tr>
+                        <td>{this.state.result_list[i].name}</td>
+                        <td><Link to="/detail" onClick={(e) => this.onClick(this.state.result_list[i].rid, this.state.result_list[i].rname)}>{this.state.result_list[i].rname}</Link></td>
+                        <td>{this.state.result_list[i].cuisine}</td>
+                    </tr>
+                );
+            
+                table.push(children);
+                console.log("table_info------------", table);
+                if (total == this.state.size) {
+                    page_info[cur.toString()] = table;
+                    cur += 1;
+                    total = 0;
+                    table = []
+                }
+            }
+        }
+        if (table.length != 0) {
+            page_info[cur.toString()] = table;
+            cur += 1
+        }
+        cur -= 1
+        console.log("page_info------------", page_info);
+        // this.setState({
+        //     pages: cur
+        // });
+        return this.helper(cur, page_info);
+
+        return page_info[1];
+        
     }
 
     createTable = () => {
@@ -103,13 +177,20 @@ class Search extends Component {
                     <select onChange={(e) => this.setState({cuisine_type: e.target.value})}>
                         <option value="All" selected>All</option>
                         {this.createSelect()}
-                    </select>
+                    </select><br/>
 
-                    <div className="search_result">
+                    {this.createPages()}
+                    {/* <div>
+                        <table class="table">
+                            {this.createPages()}
+                        </table>
+                    </div> */}
+
+                    {/* <div className="search_result">
                         <table class="table">
                             {this.createTable()}
                         </table>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         );
